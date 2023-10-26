4b36113fde645a68594a89148371b67a68d63c46
chica_alex_gitthub@gmx.es <chica_alex_gitthub@gmx.es>
2023-10-26 14:11:22 +0200

    commit 2

--- src/main/java/com/project/Controller0.java
+++ src/main/java/com/project/Controller0.java
@@ -20,9 +20,9 @@ public class Controller0 {
     @FXML
     private Label percentatge0, percentatge1, percentatge2;
     
-    private ExecutorService executor0 = Executors.newFixedThreadPool(1);
-    private ExecutorService executor1 = Executors.newFixedThreadPool(1);
-    private ExecutorService executor2 = Executors.newFixedThreadPool(1);
+    private final ExecutorService executor0 = Executors.newFixedThreadPool(1);
+    private final ExecutorService executor1 = Executors.newFixedThreadPool(1);
+    private final ExecutorService executor2 = Executors.newFixedThreadPool(1);
 
 
     @FXML
@@ -32,56 +32,49 @@ public class Controller0 {
 
     @FXML
     private void runTask0() {
-
-        backgroundTask(0, executor0, percentatge0);
+        backgroundTask(executor0, percentatge0);
         //backgroundTask(1, executor0, percentatge0);
     }
     @FXML
     private void runTask1() {
-        backgroundTask(0, executor1, percentatge1);
+        backgroundTask(executor1, percentatge1);
         //backgroundTask(1, executor1, percentatge1);
     }
     @FXML
     private void runTask2(){
-        backgroundTask(0, executor2, percentatge2);
+        backgroundTask(executor2, percentatge2);
         //backgroundTask(1, executor2, percentatge2);
     }
 
-    private void backgroundTask(int index, ExecutorService executor, Label label) {
+    private void backgroundTask(ExecutorService executor, Label label) {
         // Executar la tasca
         executor.submit(() -> {
             try {
                 for (int i = 0; i <= 100; i++) {
                     final int currentValue = i;
-    
-                    if (index == 0) {
-                        // Actualitzar el Label en el fil d'aplicació de l'UI
-                        Platform.runLater(() -> {
-                            label.setText(String.valueOf(currentValue) + "%");
-                        });
-                        Thread.sleep(100);
 
-                    }
 
-                    if (index == 1) {
-                        // Actualitzar el Label en el fil d'aplicació de l'UI
-                        Platform.runLater(() -> {
-                            label.setText(String.valueOf(currentValue) + "%");
-                        });
-                        Thread.sleep(40);
-                    }
 
-                    System.out.println("Updating label: " + index + ", Value: " + currentValue);
+                    // Actualitzar el Label en el fil d'aplicació de l'UI
+                    Platform.runLater(() -> {
+                        label.setText(String.valueOf(currentValue) + "%");
+                    });
+                    Thread.sleep(100);
+
+
+                    System.out.println("Updating label: " + ", Value: " + currentValue);
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
+
     }
     
     // Aquesta funció la cridaries quan vulguis tancar l'executor (per exemple, quan tanquis la teva aplicació)
     public void stopExecutor(ExecutorService executor) {
         executor.shutdown();
     }
+
 
 }
